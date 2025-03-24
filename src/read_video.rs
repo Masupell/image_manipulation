b5bb4009c4c863e8b660577db5fd06da04f6d77c
Masupell <mvelasquez2106@gmail.com>
2025-03-24 18:01:25 +0100

    stand for pc

--- src/read_video.rs
+++ src/read_video.rs
@@ -1,4 +1,6 @@
-use ffmpeg_next as ffmpeg;
+use anyhow::Error;
+use ffmpeg_next::{self as ffmpeg, codec::traits::Decoder};
+use ffmpeg::{codec, format, decoder};
 
 pub fn read()
 {
@@ -16,7 +18,24 @@ pub fn read()
 
             let video_stream = input.streams().best(ffmpeg::media::Type::Video).ok_or_else(|| anyhow::anyhow!("No video stream found")).unwrap();
             println!("Video Stream: Index: {}, Codec: {:?}", video_stream.index(), video_stream.parameters().id());
+
+            let test = open_codec_context(&video_stream);
         },
         Err(e) => println!("Failed to open Video: {}", e),
     }
+}
+
+fn open_codec_context(video_stream: &ffmpeg::format::stream::Stream) -> Result<ffmpeg::codec::Context, ffmpeg::Error> 
+{
+    let codec_params = video_stream.parameters();
+
+    let codec_id = codec_params.id();
+
+    let codec = decoder::find(codec_id).ok_or(ffmpeg::Error::DecoderNotFound)?;
+
+    // let mut codec_context = 
+    
+    codec_context.open()?;
+
+    Ok(codec_context)
 }
