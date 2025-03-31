2b9687abb63eeb79611947893433acc55ef4bfed
Masupell <mvelasquez2106@gmail.com>
2025-03-31 10:44:40 +0200

    ffmpeg Frame Decoder

--- src/read_video.rs
+++ src/read_video.rs
@@ -10,32 +10,44 @@ pub fn read()
 
     match ffmpeg::format::input(&path)
     {
-        Ok(input) => 
+        Ok(mut input) => 
         {
             println!("Opened video: {}", path);
             println!("Format: {}", input.format().name());
             println!("Duration: {} seconds", input.duration() as f64 / ffmpeg::ffi::AV_TIME_BASE as f64);
 
-            let video_stream = input.streams().best(ffmpeg::media::Type::Video).ok_or_else(|| anyhow::anyhow!("No video stream found")).unwrap();
-            println!("Video Stream: Index: {}, Codec: {:?}", video_stream.index(), video_stream.parameters().id());
+            let video_stream_index = input.streams().best(ffmpeg::media::Type::Video).map(|s| s.index()).ok_or_else(|| anyhow::anyhow!("No Video-Stream found")).unwrap();
 
-            let test = open_codec_context(&video_stream);
+            let mut decoder = open_codec_context(&input.stream(video_stream_index).unwrap()).unwrap();
+
+            for (stream_index, packet) in input.packets() 
+            {
+                if stream_index.index() == video_stream_index 
+                {
+                    if decoder.send_packet(&packet).is_ok() 
+                    {
+                        let mut frame = ffmpeg::frame::Video::empty();
+
+                        while decoder.receive_frame(&mut frame).is_ok() 
+                        {
+                            println!("Decoded frame: {}x{}", frame.width(), frame.height());
+                        }
+                    }
+                }
+            }
         },
         Err(e) => println!("Failed to open Video: {}", e),
     }
 }
 
-fn open_codec_context(video_stream: &ffmpeg::format::stream::Stream) -> Result<ffmpeg::codec::Context, ffmpeg::Error> 
+fn open_codec_context(video_stream: &ffmpeg::format::stream::Stream) -> Result<ffmpeg::decoder::Video, ffmpeg::Error> 
 {
     let codec_params = video_stream.parameters();
 
-    let codec_id = codec_params.id();
+    let mut context = ffmpeg::codec::Context::new();
+    context.set_parameters(codec_params)?;
 
-    let codec = decoder::find(codec_id).ok_or(ffmpeg::Error::DecoderNotFound)?;
+    let decoder = context.decoder().video()?;
 
-    // let mut codec_context = 
-    
-    codec_context.open()?;
-
-    Ok(codec_context)
+    Ok(decoder)
 }
